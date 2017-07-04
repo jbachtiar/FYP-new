2c72734e83fcf174fa5bb6dadf877e0e669f2ecf
clrisssa <its.clarissa@gmail.com>
2017-07-04 20:56:37 +0800

    added home and app component to routing

--- app/legit-app/src/app/app.routing.ts
+++ app/legit-app/src/app/app.routing.ts
@@ -1,9 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component'
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component'
+import { HomeComponent } from './home/home.component'
 
 const appRoutes: Routes = [
+    { path: '', component: AppComponent},
+    { path: 'home', component: HomeComponent},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 ];
