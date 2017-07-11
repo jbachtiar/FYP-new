1840003243be2147a4209b5aa9b4b52ff6cdbffb
clrisssa <its.clarissa@gmail.com>
2017-07-12 00:47:06 +0800

    added routing to product detail and paginator

--- app/legit-app/src/app/product-list/product-list.component.ts
+++ app/legit-app/src/app/product-list/product-list.component.ts
@@ -1,21 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../services/product.service'
+import { ProductService } from '../product.service'
+import { PagerService } from '../pager.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
-  providers: [ProductService]
+  providers: [ProductService, PagerService]
 })
 export class ProductListComponent implements OnInit {
+  //list of products 
   private products = []
 
-  constructor(private productService:ProductService) { }
+  // pager object
+  pager: any = {};
+
+  // paged items
+  pagedProducts: any[];
+
+  constructor(
+    private productService:ProductService, 
+    private pagerService: PagerService,
+    private router: Router) { }
     
   ngOnInit() {
     this.productService.getProductList().subscribe(
       products => {
         this.products = products;
+        //initialise paginator 
+        this.setPage(1);
       });
+  }
+
+  onSelect(product): void {
+		let link = ['/products', product.id];
+		this.router.navigate(link);
+	}
+
+  setPage(page: number) {
+        if (page < 1 || page > this.pager.totalPages) {
+            return;
+        }
+ 
+        // get pager object from service
+        this.pager = this.pagerService.getPager(this.products.length, page);
+ 
+        // get current page of items
+        this.pagedProducts = this.products.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
 }
