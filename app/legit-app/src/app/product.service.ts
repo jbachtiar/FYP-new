b22c13b9c07c85a42b78a2da259188e962e7df99
clrisssa <its.clarissa@gmail.com>
2017-07-12 00:46:30 +0800

    moved product service to app root folder

--- app/legit-app/src/app/product.service.ts
+++ app/legit-app/src/app/product.service.ts
@@ -20,78 +20,58 @@ export class ProductService {
                 "price": "100"
                 },
                 {
-                "id": "1",
-                "name": "Product 1",
-                "price": "100"
-                },
-                {
-                "id": "1",
+                "id": "2",
                 "name": "Product 2",
                 "price": "100"
                 },
                 {
-                "id": "1",
+                "id": "3",
+                "name": "Product 3",
+                "price": "100"
+                },
+                {
+                "id": "4",
                 "name": "Product 4",
                 "price": "100"
                 },
                 {
-                "id": "1",
+                "id": "5",
                 "name": "Product 5",
                 "price": "100"
                 },
                 {
-                "id": "1",
+                "id": "6",
                 "name": "Product 6",
                 "price": "100"
                 },
                 {
-                "id": "1",
+                "id": "7",
                 "name": "Product 7",
                 "price": "100"
                 },
                 {
-                "id": "1",
+                "id": "8",
                 "name": "Product 8",
                 "price": "100"
                 },
                 {
-                "id": "1",
+                "id": "9",
                 "name": "Product 9",
                 "price": "100"
                 },
                 {
-                "id": "1",
+                "id": "10",
                 "name": "Product 10",
                 "price": "100"
                 },
                 {
-                "id": "1",
+                "id": "11",
                 "name": "Product 11",
                 "price": "100"
                 },
                 {
-                "id": "1",
+                "id": "12",
                 "name": "Product 12",
-                "price": "100"
-                },
-                {
-                "id": "1",
-                "name": "Product 13",
-                "price": "100"
-                },
-                {
-                "id": "1",
-                "name": "Product 14",
-                "price": "100"
-                },
-                {
-                "id": "1",
-                "name": "Product 15",
-                "price": "100"
-                },
-                {
-                "id": "1",
-                "name": "Product 16",
                 "price": "100"
                 }
             ]
