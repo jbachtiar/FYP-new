589441168712466b9b631f4b698532254c85282a
Clarissa Poedjiono <its.clarissa@gmail.com>
2017-07-15 03:17:48 +0800

    guard to call login popup instead of login page

--- app/legit-app/src/app/guard/auth.guard.ts
+++ app/legit-app/src/app/guard/auth.guard.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
+import { LoginPopupComponent } from '../login/login-popup.component';
+
+import { DialogService } from "ng2-bootstrap-modal";
+
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router) { }
+    constructor(
+        private router: Router,
+        private dialogService: DialogService) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         if (localStorage.getItem('token')) {
@@ -12,8 +18,20 @@ export class AuthGuard implements CanActivate {
             return true;
         }
 
+        let disposable = this.dialogService.addDialog(LoginPopupComponent, {
+            title:'Login', 
+            message:''})
+          .subscribe((isConfirmed)=>{
+              //We get dialog result
+              if(isConfirmed) {
+                  window.location.reload();
+              }
+              else {
+                  //do nothing
+              }
+          });
         // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+        // this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
         return false;
     }
 }
