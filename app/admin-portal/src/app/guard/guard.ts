bb3e1678a8dc96bd8e48a853d5cf629fdbf3fda6
Jeremy Bachtiar <jbachtiar.2015@smu.edu.sg>
2017-07-27 14:43:27 +0800

    fix admin portal guard.ts

--- app/admin-portal/src/app/guard/guard.ts
+++ app/admin-portal/src/app/guard/guard.ts
@@ -7,18 +7,14 @@ export class AuthGuard implements CanActivate {
     constructor(private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-<<<<<<< HEAD
-        if (localStorage.getItem('currentUser')) {
-=======
         //verify if have token
         if (localStorage.getItem('token')) {
->>>>>>> admin-debug
             // logged in so return true
             return true;
         }
 
         // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+        this.router.navigate(['/']);
         return false;
     }
 }
