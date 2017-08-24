9408579eccb20b5688b210304f3fc7d6378c7058
Jeremy Bachtiar <jbachtiar.2015@smu.edu.sg>
2017-08-25 00:13:53 +0800

    STAFF and STAFF_ROLE DAO done basic CRUD and get all

--- app/backend-app/FYP-backend/src/java/dao/StaffRoleDAO.java
+++ app/backend-app/FYP-backend/src/java/dao/StaffRoleDAO.java
@@ -3,93 +3,168 @@
 // * To change this template file, choose Tools | Templates
 // * and open the template in the editor.
 // */
-//package dao;
+package dao;
 //
-//import database.ConnectionManager;
-//import entity.StaffRole;
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
+import database.ConnectionManager;
+import entity.StaffRole;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 //
 ///**
 // *
-// * @author JeremyBachtiar
+// * @author Ong Yi Xuan
 // */
-//public class StaffRoleDAO {
-//    
-//    public static StaffRole retrieveRoleByCode(String roleCode) {
-//        Connection conn = null;
-//        PreparedStatement stmt = null;
-//        ResultSet rs = null;
-//        StaffRole staffRole = null;
-//        
-//        String sql = "SELECT * FROM staff_role WHERE role_code = ? "; 
-//        try {
-//            conn = ConnectionManager.getConnection();
-//            stmt = conn.prepareStatement(sql);
-//            stmt.setString(1, roleCode);
-//            rs = stmt.executeQuery();
-//            
-//            while (rs.next()) {
-//                
-//                String roleName = rs.getString(2);
-//                
-//              
-//                staffRole = new StaffRole(roleCode, roleName);
-//             
-//            }
-//
-//        } catch (SQLException ex) {
-//            handleSQLException(ex, sql);
-//        } finally {
-//            ConnectionManager.close(conn, stmt, rs);
-//        }
-//        return staffRole;
-//    }
-//    
-//    public static ArrayList<StaffRole> retrieveAllRole() {
-//        Connection conn = null;
-//        PreparedStatement stmt = null;
-//        ResultSet rs = null;
-//        StaffRole staffRole = null;
-//        ArrayList<StaffRole> staffList = null;
-//        
-//        String sql = "SELECT * FROM staff_role"; 
-//        try {
-//            conn = ConnectionManager.getConnection();
-//            stmt = conn.prepareStatement(sql);
-//            rs = stmt.executeQuery();
-//            
-//            while (rs.next()) {
-//                String roleCode = rs.getString(1);
-//                String roleName = rs.getString(2);
-//                
-//              
-//                staffRole = new StaffRole(roleCode, roleName);
-//                staffList.add(staffRole);
-//            }
-//
-//        } catch (SQLException ex) {
-//            handleSQLException(ex, sql);
-//        } finally {
-//            ConnectionManager.close(conn, stmt, rs);
-//        }
-//        return staffList;
-//    }
-//    
-//   
-//    
-//    private static void handleSQLException(SQLException ex, String sql, String... parameters) {
-//        String msg = "Unable to access data; SQL=" + sql + "\n";
-//        for (String parameter : parameters) {
-//            msg += "," + parameter;
-//        }
-//        Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, msg, ex);
-//    }
-//}
-//    
-//
+public class StaffRoleDAO {
+
+    public String addStaffRole(StaffRole sr) throws SQLException{
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+       
+        String sql = "INSERT INTO STAFF_ROLE VALUES (?,?)";
+        if(getStaffRoleById(sr.getRoleId())!=null){
+            
+            try {
+
+                conn = ConnectionManager.getConnection();
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, sr.getRoleId());
+                stmt.setString(2, sr.getRoleName());
+                
+                rs = stmt.executeQuery();
+                
+            } finally {
+                ConnectionManager.close(conn, stmt, rs);
+            }
+        }else{
+            return "Staff role already exist";
+        }
+        
+        return "Success";
+    }
+    
+    public StaffRole getStaffRoleById(String roleId) throws SQLException{
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        StaffRole result = null;
+        
+        String sql = "SELECT * FROM STAFF_ROLE WHERE ROLE_ID = ?";
+        
+        try {
+            
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, roleId);
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                String roleName = rs.getString("ROLE_NAME");
+                
+                result = new StaffRole(roleId, roleName);
+            }
+
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        
+        return result;
+        
+    }
+    
+    public ArrayList<StaffRole> getAllStaffRole() throws SQLException{
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<StaffRole> roleList = new ArrayList<StaffRole>();
+        
+        String sql = "SELECT * FROM STAFF_ROLE";
+        try {
+            
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                String roleId = rs.getString("ROLE_ID");
+                String roleName = rs.getString("ROLE_NAME");
+                
+                StaffRole sr = new StaffRole(roleId, roleName);
+                roleList.add(sr);
+            }
+
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        
+        return roleList;
+        
+    }
+    
+    public String deleteStaffRoleIdById(String id) throws SQLException{
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+       
+       String sql = "DELETE FROM STAFF_ROLE WHERE ROLE_ID = ?";
+        
+        try {
+
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, id);
+
+            rs = stmt.executeQuery();
+
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+
+        return "Success";
+    }
+    public String updateStaffRole(StaffRole sr)throws SQLException{
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+       
+        String sql = "UPDATE STAFF_ROLE SET ROLE_NAME = ? WHERE ROLE_ID = ?";
+        
+        if(getStaffRoleById(sr.getRoleId())!=null){
+            
+            try {
+
+                conn = ConnectionManager.getConnection();
+                stmt = conn.prepareStatement(sql);
+                
+                stmt.setString(1, sr.getRoleName());
+                stmt.setString(2, sr.getRoleId());
+                
+                rs = stmt.executeQuery();
+                
+            } finally {
+                ConnectionManager.close(conn, stmt, rs);
+            }
+        }else{
+            return "Staff role does not exist";
+        }
+        
+        return "Success";
+    }
+    
+    private static void handleSQLException(SQLException ex, String sql, String... parameters) {
+        String msg = "Unable to access data; SQL=" + sql + "\n";
+        for (String parameter : parameters) {
+            msg += "," + parameter;
+        }
+        Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, msg, ex);
+    }
+    
+}
