81188a169f399c9f399da6d7ae81c62c0838702f
Jeremy Bachtiar <jbachtiar.2015@smu.edu.sg>
2017-08-28 22:01:17 +0800

    Completed Order DAO
    Completed add, get by id, get by email

--- app/backend-app/FYP-backend/src/java/dao/OrderDAO.java
+++ app/backend-app/FYP-backend/src/java/dao/OrderDAO.java
@@ -12,11 +12,13 @@ package dao;
 import database.ConnectionManager;
 import entity.Address;
 import entity.Order;
+import entity.OrderItem;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public class OrderDAO {
 //    
@@ -57,10 +59,52 @@ public class OrderDAO {
 //        return address.toArray(new Address[address.size()]);
 //    }
     
-    public Order getOrderById(int orderId) throws SQLException {
+    public String addOrder(Order o, String email) throws SQLException{
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
+        int orderId = getNextOrderId();
+        OrderItem[] orderItems =  o.getOrderItems();
+        
+        for(OrderItem oI: orderItems){
+            OrderItemDAO orderItemDAO = new OrderItemDAO();
+            String result = orderItemDAO.addOrderItems(orderId, oI);
+        }
+        String sql = "INSERT INTO ORDER VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, orderId);
+            stmt.setTimestamp(2, getCurrentTimeStamp());
+            stmt.setDouble(3, o.getNetAmt());
+            stmt.setDouble(4, o.getPromoDiscAmt());
+            stmt.setString(5, o.getAddress().getAddressLine());
+            stmt.setString(6, o.getAddress().getCity());
+            stmt.setString(7, o.getAddress().getCountry());
+            stmt.setString(8, o.getAddress().getPostalCode());
+            stmt.setString(9, o.getPaymentRefNo());
+            stmt.setString(10, email);
+            stmt.setInt(11, o.getPromoCode().getPromoCodeId());
+            
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+            }
+        } finally {
+            ConnectionManager.close(conn);
+        }
+        
+        return "Success";
+        
+    }
+    
+    public ArrayList<Order> getOrderById(int orderId) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Order> orderList = new ArrayList<Order>();
         Order order = null;
         
         String sql = "SELECT * FROM order WHERE order_id= ?";
@@ -86,6 +130,45 @@ public class OrderDAO {
                 Address a = new Address(0, addressLine, city, country, postalCode, "N");
                 OrderItemDAO orderItemDao = new OrderItemDAO();
                 order = new Order(orderId, orderDate, netAmt, promoDiscAmt, a, paymentRefNo, pcDao.getPromoCodeById(promoCode), orderItemDao.getOrderItemsByOrderId(orderId), orderLog.getOrderStatusByOrderId(orderId));
+                orderList.add(order);
+            }
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        
+        return orderList;
+    }
+    
+    public Order getOrderByEmail(String email) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        
+        Order order = null;
+        
+        String sql = "SELECT * FROM order WHERE EMAIL = ?";
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, email);
+            rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                int orderId = rs.getInt(1);
+                Timestamp orderDate = rs.getTimestamp(2);
+                double netAmt = rs.getDouble(3);
+                double promoDiscAmt = rs.getDouble(4);
+                String addressLine = rs.getString(5);
+                String city = rs.getString(6);
+                String country = rs.getString(7);
+                String postalCode = rs.getString(8);
+                String paymentRefNo = rs.getString(9);
+                int promoCode = rs.getInt(10);
+                OrderStatusLogDAO orderLog = new OrderStatusLogDAO();
+                PromoCodeDAO pcDao = new PromoCodeDAO();
+                Address a = new Address(0, addressLine, city, country, postalCode, "N");
+                OrderItemDAO orderItemDao = new OrderItemDAO();
+                order = new Order(orderId, orderDate, netAmt, promoDiscAmt, a, paymentRefNo, pcDao.getPromoCodeById(promoCode), orderItemDao.getOrderItemsByOrderId(orderId), orderLog.getOrderStatusByOrderId(orderId));
                 
             }
         } finally {
@@ -95,6 +178,43 @@ public class OrderDAO {
         return order;
     }
     
+    public int getNextOrderId() throws SQLException {
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        
+        int nextOrderId = 0;
+
+        String sql = "SELECT MAX(ORDER_ID) AS MAX FROM ORDER";
+
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+                nextOrderId = rs.getInt("MAX") +1;
+                
+            }
+
+        } finally {
+            
+            ConnectionManager.close(conn, stmt, rs);
+            
+        }
+
+        return nextOrderId;
+        
+    }
+    
+    private java.sql.Timestamp getCurrentTimeStamp() {
+
+        java.util.Date today = new java.util.Date();
+        return new java.sql.Timestamp(today.getTime());
+
+    }
     
 //    private static void handleSQLException(SQLException ex, String sql, String... parameters) {
 //        String msg = "Unable to access data; SQL=" + sql + "\n";
