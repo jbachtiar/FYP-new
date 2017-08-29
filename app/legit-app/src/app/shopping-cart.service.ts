7814cc9ce4f15ec7f133e86fdf0e7a7018add556
clrisssa <its.clarissa@gmail.com>
2017-08-29 18:19:36 +0800

    added shared service to enable method calls across components
    removed the one in shopping cart service

--- app/legit-app/src/app/shopping-cart.service.ts
+++ app/legit-app/src/app/shopping-cart.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable, ViewChild } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { Observer } from "rxjs/Observer";
 import 'rxjs/add/operator/map'
@@ -6,7 +6,6 @@ import { CartItem } from "./cart/model/cart-item.model";
 import { ShoppingCart } from "./cart/model/shopping-cart.model";
 import { CONFIG } from './config/config.component'
 import { Http, Headers, Response } from '@angular/http';
-import { Subject } from 'rxjs/Subject';
 
 const CART_KEY = "cart";
 
@@ -18,17 +17,9 @@ export class ShoppingCartService {
   private item: CartItem;
   private cart: ShoppingCart;
   private token: string;
-  private cartMethodCallSource = new Subject<any>();
 
-    // Observable string streams
-    cartMethodCalled$ = this.cartMethodCallSource.asObservable();
 
-    // Service message commands
-    callCartMethod() {
-        this.cartMethodCallSource.next();
-    }
-
-  public constructor(private _http: Http) {
+  public constructor(private _http: Http, ) {
     this.token = localStorage.getItem('token');
     this.subscriptionObservable = new Observable<ShoppingCart>((observer: Observer<ShoppingCart>) => {
       this.subscribers.push(observer);
@@ -39,16 +30,16 @@ export class ShoppingCartService {
     });
   }
 
-  public chargeStripe(stripeToken, amount){
+  public chargeStripe(stripeToken, amount) {
     let params: URLSearchParams = new URLSearchParams();
-    let headers= new Headers();
-    headers.append (
-      'Content-type','application/x-www-form-urlencoded'
-   );
+    let headers = new Headers();
+    headers.append(
+      'Content-type', 'application/x-www-form-urlencoded'
+    );
     params.set('stripeToken', stripeToken);
     params.set('amount', amount);
-    return this._http.post(CONFIG.paymentBackendUrl+"/chargeStripe" ,params.toString(), {headers: headers} )
-    .map(res => res.json());
+    return this._http.post(CONFIG.paymentBackendUrl + "/chargeStripe", params.toString(), { headers: headers })
+      .map(res => res.json());
   }
 
   // public updateCart()
@@ -56,8 +47,8 @@ export class ShoppingCartService {
   public get(): Observable<ShoppingCart> {
     return this.subscriptionObservable;
   }
-  
-  public newCart(): void{
+
+  public newCart(): void {
     this.cart = new ShoppingCart();
     localStorage.setItem(CART_KEY, JSON.stringify(this.cart));
   }
@@ -71,11 +62,11 @@ export class ShoppingCartService {
     console.log('each price : ' + cartItem.eachPrice)
 
 
-    
+
     this.item = this.cart.items.find((p) => p.productId === cartItem.productId);
     //console.log('retrieved id: ' + this.item.productId);
     if (this.item === undefined) {
-      
+
       this.item = new CartItem();
       this.item.productId = cartItem.productId;
       this.item.quantity = cartItem.quantity;
@@ -93,7 +84,7 @@ export class ShoppingCartService {
 
       var totalPrice: number = 0;
       console.log('price before: ' + totalPrice)
-      
+
       this.item.eachPrice = cartItem.eachPrice
       console.log(JSON.stringify(this.item))
       console.log('price: ' + this.item.eachPrice);
@@ -101,64 +92,64 @@ export class ShoppingCartService {
       this.cart.items.push(this.item);
 
 
-    }else{
-        this.item.quantity += cartItem.quantity;
+    } else {
+      this.item.quantity += cartItem.quantity;
     }
 
     this.cart.items = this.cart.items.filter((cartItem) => cartItem.quantity > 0);
-    console.log('retrieved: '+ JSON.stringify(this.cart));
+    console.log('retrieved: ' + JSON.stringify(this.cart));
 
 
-    
+
     this.calculateCart(this.cart);
     this.save(this.cart);
     this.dispatch(this.cart);
   }
 
   updateCartDB(cart: ShoppingCart) {
-       
-    
-        var params = JSON.stringify(cart);
-        let headers = new Headers();
-
-        
-        headers.append ('Authorization', this.token);
-        headers.append(
-           'Content-type', 'application/json'
-        )
-
-        console.log("update cart1")
-        let url = CONFIG.updateCartBackendUrl;
-        return this._http.post(url, params, { headers: headers })
-            .subscribe(res => {
-              console.log("update cart")
-              res.json()
-              
 
 
-            });   
+    var params = JSON.stringify(cart);
+    let headers = new Headers();
 
-  }
 
-  public retrieveCartDB(){
-        let headers = new Headers();
+    headers.append('Authorization', this.token);
+    headers.append(
+      'Content-type', 'application/json'
+    )
 
-        headers.append ('Authorization', this.token);
-        
-        let url = CONFIG.cartBackendUrl + "/retrieveCart";
+    console.log("update cart1")
+    let url = CONFIG.updateCartBackendUrl;
+    return this._http.post(url, params, { headers: headers })
+      .subscribe(res => {
+        console.log("update cart")
+        res.json()
 
-        console.log("HEY IM HERE");
-        return this._http.post(url,{ headers: headers })
-            .subscribe(res => {
-              console.log("retrieved cart : " + res.json())
-            
-            });
+
+
+      });
 
   }
 
-  public updateCart(cart: ShoppingCart){
+  public retrieveCartDB() {
+    let headers = new Headers();
+
+    headers.append('Authorization', this.token);
+
+    let url = CONFIG.cartBackendUrl + "/retrieveCart";
+
+    console.log("HEY IM HERE");
+    return this._http.post(url, { headers: headers })
+      .subscribe(res => {
+        console.log("retrieved cart : " + res.json())
+
+      });
+
+  }
+
+  public updateCart(cart: ShoppingCart) {
     this.cart = cart
-    
+
     this.calculateCart(this.cart);
     this.save(this.cart);
     this.dispatch(this.cart);
@@ -172,11 +163,11 @@ export class ShoppingCartService {
   }
 
   private calculateCart(cart: ShoppingCart): void {
-    let totalPrice:number = 0;
+    let totalPrice: number = 0;
 
-    for(let item of cart.items){
-        let price = item.quantity * item.eachPrice;
-        totalPrice += price;
+    for (let item of cart.items) {
+      let price = item.quantity * item.eachPrice;
+      totalPrice += price;
     }
     cart.noOfItems = cart.items.length
     cart.totalPrice = totalPrice;
@@ -198,13 +189,13 @@ export class ShoppingCartService {
 
   private dispatch(cart: ShoppingCart): void {
     this.subscribers
-        .forEach((sub) => {
-          try {
-            sub.next(cart);
-          } catch (e) {
-            // we want all subscribers to get the update even if one errors.
-          }
-        });
+      .forEach((sub) => {
+        try {
+          sub.next(cart);
+        } catch (e) {
+          // we want all subscribers to get the update even if one errors.
+        }
+      });
   }
 
 }
