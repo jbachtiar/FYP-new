ae220a795cc2dc9086237e0f7f382bc6e91e63d7
yixuan.ong.2015 <yixuan.ong.2015@smu.edu.sg>
2017-09-11 15:52:13 +0800

    added webservice to update order status log

--- app/backend-app/FYP-backend/src/java/dao/OrderStatusLogDAO.java
+++ app/backend-app/FYP-backend/src/java/dao/OrderStatusLogDAO.java
@@ -53,6 +53,50 @@ public class OrderStatusLogDAO {
 
     }
     
+    private java.sql.Timestamp getCurrentTimeStamp() {
+
+        java.util.Date today = new java.util.Date();
+        return new java.sql.Timestamp(today.getTime());
+
+    }
     
+    public String updateOrderStatusLogByOrderId(int orderId, int previousStatusId, int newStatusId) throws SQLException {
+
+        Timestamp curr_ts = getCurrentTimeStamp();
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        PreparedStatement stmt1 = null;
+        ResultSet rs = null;
+
+        String sql = "UPDATE ORDER_STATUS_LOG SET END_TIMESTAMP = ?, DURATION_HOURS = ? WHERE ORDER_ID = ? AND STATUS_ID = ? AND DURATION_HOURS = 0";
+        String sql1 = "INSERT ORDER_STATUS_LOG (ORDER_ID, STATUS_ID, START_TIMESTAMP, END_TIMESTAMP, DURATION_HOURS) VALUES (?,?,?,?,?)";
+        
+        try {
+
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setTimestamp(1, curr_ts);
+            stmt.setDouble(2, 10);
+            stmt.setInt(3, orderId);
+            stmt.setInt(4, previousStatusId);
+            stmt.executeUpdate();
+            
+            stmt1 = conn.prepareStatement(sql1);
+            stmt1.setInt(1, orderId);
+            stmt1.setInt(2, newStatusId);
+            stmt1.setTimestamp(3, curr_ts);
+            stmt1.setTimestamp(4, curr_ts);
+            stmt1.setDouble(5, 0);
+            stmt1.executeUpdate();
+
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+            ConnectionManager.close(conn, stmt1, rs);
+        }
+
+        return "Success";
+
+    }
 
 }
