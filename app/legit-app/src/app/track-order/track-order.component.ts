b5ed6e1f6387d2ad816754f86dccaa3d5de342f0
Huiyan820 <huiyan.chen.2015@sis.smu.edu.sg>
2017-09-16 00:39:04 +0800

    completed displaying order list

--- app/legit-app/src/app/track-order/track-order.component.ts
+++ app/legit-app/src/app/track-order/track-order.component.ts
@@ -12,6 +12,8 @@ export class TrackOrderComponent implements OnInit {
   private pastOrders: any = {}
   private currentOrders: any = {}
   private counter = 0;
+  private isDisplayCurrentOrder=true
+  private isDisplayPastOrder=true
 
 
 
@@ -25,13 +27,28 @@ export class TrackOrderComponent implements OnInit {
       orders => {
         this.pastOrders = orders;
         console.log("past order" + JSON.stringify(this.pastOrders));
+        if (this.pastOrders.length==0){
+          this.isDisplayPastOrder=false;
+        }
       });
     //console.log("haha" + JSON.stringify(this.pastOrders));
      this.orderService.getCurrentOrderByCustomer(this.token).subscribe(
       orders => {
         this.currentOrders = orders;
         console.log("current order" + JSON.stringify(this.currentOrders));
+        if (this.currentOrders.length==0){
+          this.isDisplayCurrentOrder=false;
+        }
       });
 
   }
+
+  returnTheLatestOrderStatus(statusLogs){
+    let size=statusLogs.length
+    let theLastLog= statusLogs[size-1]
+    return theLastLog.orderStatus.statusName;
+
+  }
+
+
 }
