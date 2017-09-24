f24bc78cd595e6679ea44a656639b8676ff929b8
Huiyan820 <huiyan.chen.2015@sis.smu.edu.sg>
2017-09-24 20:49:30 +0800

    Linked the pattern list and product details to the new webservice

--- app/admin-portal/src/app/config/prod.ts
+++ app/admin-portal/src/app/config/prod.ts
@@ -1,7 +1,7 @@
 export const prodConfig = {
     authenticationBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/authentication/admin/login',
     registrationBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/registration/insert',
-    productCatalogueBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue',
+    productCatalogueBackendUrl:'http://localhost:8084/FYP-backend/API/ProductCatalogue/BeddingPatterns',
     patternCatalogueBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/PatternCatalogue',
     fabricCatalogueBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/FabricCatalogue',
     staffBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/staff',
