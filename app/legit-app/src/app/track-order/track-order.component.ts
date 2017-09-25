fd223e3b4a695c45180c3a2cfb82d6c10d4a4f07
Huiyan820 <huiyan.chen.2015@sis.smu.edu.sg>
2017-09-26 02:43:54 +0800

    Add side bar for order tracking

--- app/legit-app/src/app/track-order/track-order.component.ts
+++ app/legit-app/src/app/track-order/track-order.component.ts
@@ -17,7 +17,23 @@ export class TrackOrderComponent implements OnInit {
   private counter = 0;
   private isDisplayCurrentOrder = true
   private isDisplayPastOrder = true
-  private loading :boolean  = true;
+  private loading: boolean = true;
+  private isDisplayOrderList = true
+
+  private orderId: any = {};
+  private order: any = {};
+  private orderItems: any = {}
+  private statusLog: any = {}
+  private statusId: number;
+
+  color = 'warn';
+  mode = 'buffer';
+  value = 100;
+  bufferValue = 100;
+  isDisabled: any = { 'payment': false, 'production': false, 'packaging': false, 'preparation': false, 'shipped': false, 'completed': false };
+  private map: any = { 1: 'payment', 2: 'production', 3: 'packaging', 4: 'preparation', 5: 'shipped', 6: 'completed' }
+  pulse: any = { 'payment': false, 'production': false, 'packaging': false, 'preparation': false, 'shipped': false, 'completed': false };
+
 
 
   constructor(private orderService: OrderService, private router: Router, private shardService: SharedService) {
@@ -50,13 +66,13 @@ export class TrackOrderComponent implements OnInit {
 
   }
 
- /* returnTheLatestOrderStatus(statusLogs) {
-    let size = statusLogs.length
-    let theLastLog = statusLogs[size - 1]
-    return theLastLog.orderStatus.statusName;
-
-  }
-  */
+  /* returnTheLatestOrderStatus(statusLogs) {
+     let size = statusLogs.length
+     let theLastLog = statusLogs[size - 1]
+     return theLastLog.orderStatus.statusName;
+ 
+   }
+   */
   returnTheLatestOrderStatus(statusLogs) {
     let status = statusLogs[0];
     console.log("STATUS: " + status)
@@ -75,15 +91,45 @@ export class TrackOrderComponent implements OnInit {
     console.log("MOST CURRENT: " + mostCurrentTimestamp)
     //    this.order[0]['currentStatus'] = currentStatus.orderStatus.statusName;
 
-    return  currentStatus.orderStatus.statusName;
+    return currentStatus.orderStatus.statusName;
 
   }
 
   viewOrder(orderId) {
-    let link = ['profile-sidebar', orderId];
-    this.router.navigate(link);
-    
+    // let link = ['profile-sidebar', orderId];
+    // this.router.navigate(link);
+    this.isDisplayOrderList = false;
+
+    this.orderService.getOrderById(orderId).subscribe(orders => {
+      this.order = orders;
+      this.orderItems = orders[0].orderItems;
+      this.statusLog = orders[0].statusLogs;
+      if (this.statusLog.length > 0) {
+        this.returnTheLatestOrderStatus(this.statusLog);
+        if (this.statusId != 6) {
+          this.isDisabled[this.map[6]] = true
+          this.bufferValue -= 4.1555
+
+          //to colour the icons with the completed statuses only and adjust progress bar buffer and value
+          for (var i = 5; i > this.statusId; i--) {
+            this.isDisabled[this.map[i]] = true;
+            this.bufferValue -= 16.569
+            console.log("this.value = " + this.value)
+          }
+          this.value = this.bufferValue - 16.569
+          this.bufferValue -= 16.569
+
+          //to make the icon of ongoing status pulse
+          this.pulse[this.map[this.statusId]] = true;
+        }
+
+      }
+
+    });
+
   }
+
+
 
 
 
