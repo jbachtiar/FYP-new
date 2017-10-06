09971ee716ef55f7f08d278faf62a84e50bf4bc5
yixuan.ong.2015 <yixuan.ong.2015@smu.edu.sg>
2017-10-06 13:58:29 +0800

    added update delete and getById for fabric

--- app/backend-app/FYP-backend/src/java/webServices/FabricService.java
+++ app/backend-app/FYP-backend/src/java/webServices/FabricService.java
@@ -14,10 +14,12 @@ import java.sql.SQLException;
 import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
@@ -69,6 +71,101 @@ public class FabricService {
 
         String finalJsonOutput = gson.toJson(jsonOutput);
         return finalJsonOutput;
+    }
+    
+    @OPTIONS
+    @PermitAll
+    @Path("/update")
+    public void optionsUpdate() {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        response.setHeader("Access-Control-Allow-Headers", "authorization");
+
+    }
+    
+    @POST
+    @Path("/update")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateFabric(@FormParam("fabric") String json) {
+        
+        FabricDAO fDAO = new FabricDAO();
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
+        Gson gs = new Gson();
+        Fabric fabricToUpdate = gs.fromJson(json, Fabric.class);
+        
+        Gson gson = new GsonBuilder().create();
+        JsonObject jsonOutput = new JsonObject();
+
+        try {
+                System.out.println("JSON: " + json);
+                jsonOutput.addProperty("status", "200");
+                fDAO.updateFabric(fabricToUpdate);
+
+        } catch (SQLException e) {
+            System.out.println(e);
+            jsonOutput.addProperty("status", "500");
+
+        }
+
+        String finalJsonOutput = gson.toJson(jsonOutput);
+        return finalJsonOutput;
+    }
+    
+    @GET
+    @Path("/delete")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String deleteFabric(@QueryParam("fabricId") int fabricId) {
+
+        FabricDAO fDAO = new FabricDAO();
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        
+        Gson gson = new GsonBuilder().create();
+        JsonObject jsonOutput = new JsonObject();
+
+        try {
+                jsonOutput.addProperty("status", "200");
+                fDAO.deleteFabricById(fabricId);
+
+        } catch (SQLException e) {
+            System.out.println(e);
+            jsonOutput.addProperty("status", "500");
+
+        }
+
+        String finalJsonOutput = gson.toJson(jsonOutput);
+        return finalJsonOutput;
+    }
+    
+    @GET
+    @Path("/GetFabricById")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Fabric getFabricById(@QueryParam("fabricId") int fabricId) {
+        
+        FabricDAO fDAO = new FabricDAO();
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        
+        Gson gson = new GsonBuilder().create();
+        JsonObject jsonOutput = new JsonObject();
+
+        try {
+                jsonOutput.addProperty("status", "200");
+                Fabric f = fDAO.getFabricById(fabricId);
+                return f;
+
+        } catch (SQLException e) {
+            System.out.println(e);
+            jsonOutput.addProperty("status", "500");
+            return null;
+
+        }
+
+        //String finalJsonOutput = gson.toJson(jsonOutput);
+        //return finalJsonOutput;
+        
     }
     
 }
