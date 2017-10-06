da5cce1587bd3ae75919092f18fedc1d5a3229cc
Huiyan820 <huiyan.chen.2015@sis.smu.edu.sg>
2017-10-06 13:00:21 +0800

    added edit for superuser

--- app/admin-portal/src/app/order-details-superuser/order-details-superuser.component.ts
+++ app/admin-portal/src/app/order-details-superuser/order-details-superuser.component.ts
@@ -8,28 +8,48 @@ import { OrderService } from '../services/order.service'
   styleUrls: ['./order-details-superuser.component.css']
 })
 export class OrderDetailsSuperuserComponent implements OnInit {
+
   private orderId: any = {};
   private order: any = {};
+  private user: any = {};
   private orderItems: any = {};
-  private status: any={};
+  private status: any = {};
+  private index: number = 0;
+  orderStatusSize: any;
+  statusMap: any = { 'Payment Received': 1, 'In Production': 2, 'Packaging': 3, 'Pending for Shipment': 4, 'Shipped': 5, 'Completed': 6, 'Cancelled': 7 }
+  statusMenu = ["Payment Received", "In Production", "Packaging", "Pending for Shipment", "Shipped", "Completed", "Cancelled"];
+  courierMenu = ["Shunfeng", "DHL"]
+  selectedStatus: any;
+  selectedCourier: any;
+  //statusMenu: any = [];
   constructor(private route: ActivatedRoute, private orderService: OrderService) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.orderId = params['orderId']; // grab the order id
       this.orderService.getOrderById(this.orderId).subscribe(orders => {
-      
-        this.order= orders[0];
-        this.status=this.order.statusLogs[0];
-        console.log("status"+this.order.statusLogs[0].orderStatus.statusName);
+
+        this.order = orders[0];
+        this.status = this.order.statusLogs[0];
+        console.log("status" + this.order.statusLogs[0].orderStatus.statusName);
         this.orderItems = orders[0].orderItems;
+        this.selectedCourier = this.order.courierName;
+        //  console.log("selectedCourier"+this.selectedCourier);
+
       });
+
     })
+
+
+
+    // console.log(this.statusMenu);
+
+
   }
 
   returnTheLatestOrderStatus() {
- 
-  
+
+
     //console.log("STATUS: " + status)
     let currentStatus = this.status
     let mostCurrentTimestamp = this.status.startTimeStamp;
@@ -44,7 +64,28 @@ export class OrderDetailsSuperuserComponent implements OnInit {
       }
     }
 
-     return  currentStatus.orderStatus.statusName;
+    return currentStatus.orderStatus.statusName;
+
+  }
+
+  editOrder() {
+
+
+    let newOder = {
+      "orderId": this.orderId,
+      "newStatusId": this.statusMap[this.selectedStatus],
+      "Timestamp": this.order.order_TimeStamp,
+      "netAmt": this.order.netAmt,
+      "promoDiscAmt": this.order.promoDiscAmt,
+      "address": this.order.address,
+      "paymentRefNo": this.order.paymentRefNo,
+      "promoCode": this.order.promoCode,
+      "orderItems": this.order.orderItems,
+      "statusLogs": this.order.statusLogs,
+      "courierName": this.order.courierName,
+      "trackingNo": this.order.trackingNo
+
+    }
 
   }
 
