d30e6e3471124302cac5c9b04856da2fca051b7a
Sheryl Chong <mechong.2015@sis.smu.edu.sg>
2017-10-09 01:17:23 +0800

    edited prod link

--- app/legit-app/src/app/config/prod.ts
+++ app/legit-app/src/app/config/prod.ts
@@ -2,12 +2,12 @@ export const prodConfig = {
     authenticationBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/authentication/login',
     registrationBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/registration',
     productCatalogueBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue',
-    productListBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue/uniquePatterns',
+    productListBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue/BeddingPatterns',
     profileBackendUrl:'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/profile',
-    quickViewBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue/product',
+    quickViewBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue/GetBedding',
     fabricBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/FabricCatalogue/fabrics',
     productDetailsBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue/customization',
-    filteredProductListBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue/filtersort',
+    filteredProductListBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue/FilterBeddingPatterns',
     searchedProductListBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue/search',
     getCartItemBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/Cart/items',
     updateCartItemBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/Cart/updateCartItems',
@@ -15,9 +15,9 @@ export const prodConfig = {
     clearCartBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/Cart/delete',
     getCartTotalPriceBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/Cart/price',
     cartBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/Cart',
-    paymentBackendUrl: 'http://localhost:8084/FYP-backend/API/Payment',
-    addressBackendUrl: 'http://localhost:8084/FYP-backend/API/AddressService',
-    filterBackendUrl: 'http://localhost:8084/FYP-backend/API/ProductCatalogue/GetFilters',
-    orderBackendUrl: 'http://localhost:8084/FYP-backend/API/OrderService'
+    paymentBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/Payment',
+    addressBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/AddressService',
+    filterBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/ProductCatalogue/GetFilters',
+    orderBackendUrl: 'http://ec2-52-35-18-15.us-west-2.compute.amazonaws.com/FYP-backend/API/OrderService'
     
 }
