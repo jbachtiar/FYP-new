a5e5b3ae69693d3c6b855ed040b430e4c4e87cfe
clrisssa <its.clarissa@gmail.com>
2017-11-18 22:04:43 +0800

    UI improvement at my account sidebar

--- app/legit-app/src/app/myaccount/myaccount.component.ts
+++ app/legit-app/src/app/myaccount/myaccount.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {HashLocationStrategy, Location, LocationStrategy} from '@angular/common';
 
 @Component({
   selector: 'app-myaccount',
   templateUrl: './myaccount.component.html',
-  styleUrls: ['./myaccount.component.css']
+  styleUrls: ['./myaccount.component.css'],
+  providers: [Location, {provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class MyaccountComponent implements OnInit {
+  @Input()
+  selectedItem: string = ""
+  
   public menuItems: any[];
   private account: boolean = false;
   private addressBook: boolean = false;
   private track: boolean = false;
-
-  constructor() { }
+  location: Location; 
+  private color: any = { 'profile': 'white', 'address': '#B0A171', 'track': '#B0A171' }
+  private fontColor: any = { 'profile': 'black', 'address': 'white', 'track': 'white' }
+  
+  constructor(location: Location) { this.location = location }
 
   ngOnInit() {
     this.track = false;
@@ -21,18 +28,27 @@ export class MyaccountComponent implements OnInit {
   }
 
   manageAccount() {
+    this.color = { 'profile': 'white', 'address': '#B0A171', 'track': '#B0A171' }
+    this.fontColor = { 'profile': 'black', 'address': 'white', 'track': 'white' }
+    
     this.track = false;
     this.account = true;
     this.addressBook = false
   }
 
   manageAddressBook() {
+    this.color = { 'profile': '#B0A171', 'address': 'white', 'track': '#B0A171' }
+    this.fontColor = { 'profile': 'white', 'address': 'black', 'track': 'white' }
+    
     this.track = false;
     this.account = false;
     this.addressBook = true;
   }
 
   trackOrder() {
+    this.color = { 'profile': '#B0A171', 'address': '#B0A171', 'track': 'white' }
+    this.fontColor = { 'profile': 'white', 'address': 'white', 'track': 'black' }
+    
     this.account = false;
     this.track = true;
     this.addressBook = false;
